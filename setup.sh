6c7204db2bfbab772a1904237e9f781a943e759f
pushkaradsule <pushkar.adsule@gmail.com>
2016-03-02 11:37:25 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,18 +15,16 @@ usage() {
 }
 
 # Check to make sure the correct number of arguments is passed.
-if [ "$#" -ne 4 ]; 
+if [ "$#" -ne 3 ]; 
 then
     usage
     exit
 fi
 
 HOSTNAME=$1
-ENCRYPT=$2
-PRIVATE_IP1=$3
-PRIVATE_IP2=$4
+PRIVATE_IP1=$2
+PRIVATE_IP2=$3
 
 sed -i -- "s/__NODE_NAME__/$HOSTNAME/g" /root/consul_demo/config.json
-sed -i -- "s/__ENCRYPT__/$ENCRYPT/g" /root/consul_demo/config.json
 sed -i -- "s/__BOOTSTRAP_PRIVATE_IP__/$PRIVATE_IP1/g" /root/consul_demo/config.json
 sed -i -- "s/__NON_BOOTSTRAP_PRIVATE_IP__/$PRIVATE_IP2/g" /root/consul_demo/config.json
